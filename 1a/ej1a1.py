f8ad281dd280ddfcc59d2b5ed6a9de3f22e566e7
Iripollb <iripollb@yahoo.es>
2025-11-26 19:43:25 +0100

    Ejercico 1a1 hecho

--- 1a/ej1a1.py
+++ 1a/ej1a1.py
@@ -18,30 +18,19 @@ Ejemplo:
     Salida:
     55
 
-Enunciat:
-Implementa la funció 'fibonacci(fibonacci_number)' que contingui l'algorisme
-de Fibonacci i rebi com a paràmetre un valor numèric enter anomenat
-'fibonacci_number' i torneu el valor de la sèrie Fibonacci en aquesta posició.
-Així mateix, si el valor no és numèric, o és menor a zero, cal llançar
-una excepció ValueError("missatge"), la qual pot incloure un missatge que hi hauria de
-correspondre amb el tipus d'error durant la validació.
-
-Paràmetres:
-- fibonacci_number: Nombre enter positiu superior a 0 que representa la
-posició a la sèrie Fibonacci.
-
-Exemple:
-     Entrada:
-     fibonacci(10)
-
-     Sortida:
-     55
 
 '''
 
 def fibonacci(fibonacci_number):
-    # Write here your code
-    pass
+    if not isinstance(fibonacci_number, int):
+        raise ValueError("El número debe ser entero.")
+    elif fibonacci_number < 0:
+        raise ValueError("El número debe ser mayor a 0.")
+    else:
+        a, b = 0, 1
+    for _ in range(fibonacci_number):
+        a, b = b, a + b
+    return a
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
